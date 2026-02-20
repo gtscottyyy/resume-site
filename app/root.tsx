26bf3ef816b007326e3d38a3643ff9e175d16523
gtscottyyy <gtscottyyy@gmail.com>
2026-02-19 22:20:56 -0500

    styling facelift

--- app/root.tsx
+++ app/root.tsx
@@ -10,9 +10,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { connectToDatabase } from "~/utils/db.server";
+import tokensHref from "~/styles/tokens.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+  { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=JetBrains+Mono:wght@400;500&display=swap" },
+  { rel: "stylesheet", href: tokensHref },
 ];
 
 export async function loader() {
